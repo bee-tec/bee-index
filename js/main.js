c7e3dbe6b3d1808dd9f217f141620c8f8dd44eb7
埋桑 <wsnbtkk@163.com>
2021-02-19 16:57:37 +0800

    swiper相关调整 (#4)
    * 修复部分图片引用路径的问题
    
    * 新增swiper依赖，调整页面结构
    
    Co-authored-by: Jhon <c353373303@hotmail.com>

--- js/main.js
+++ js/main.js
@@ -31,25 +31,25 @@ $(function () {
       });
     });
     // 返回顶部
-    var offset = 300,
-      offset_opacity = 1200,
-      scroll_top_duration = 700,
-      $back_to_top = $(".toTop");
-    $(window).scroll(function () {
-      $(this).scrollTop() > offset
-        ? $back_to_top.addClass("cd-is-visible")
-        : $back_to_top.removeClass("cd-is-visible cd-fade-out");
-      if ($(this).scrollTop() > offset_opacity) {
-        $back_to_top.addClass("cd-fade-out");
-      }
-    });
-    $back_to_top.on("click", function (event) {
-      event.preventDefault();
-      $("body,html").animate(
-        {
-          scrollTop: 0,
-        },
-        scroll_top_duration
-      );
-    });
+    // var offset = 300,
+    //   offset_opacity = 1200,
+    //   scroll_top_duration = 700,
+    //   $back_to_top = $(".toTop");
+    // $(window).scroll(function () {
+    //   $(this).scrollTop() > offset
+    //     ? $back_to_top.addClass("cd-is-visible")
+    //     : $back_to_top.removeClass("cd-is-visible cd-fade-out");
+    //   if ($(this).scrollTop() > offset_opacity) {
+    //     $back_to_top.addClass("cd-fade-out");
+    //   }
+    // });
+    // $back_to_top.on("click", function (event) {
+    //   event.preventDefault();
+    //   $("body,html").animate(
+    //     {
+    //       scrollTop: 0,
+    //     },
+    //     scroll_top_duration
+    //   );
+    // });
   });
